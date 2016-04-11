41d6b99672983a3e19c73a1442beae09751c67ac
Traitortots <jker3169@740s-pl-844.local>
2016-04-11 08:25:26 -0600

    added code for TreeNode Constructor

--- NodeProject/Model/TreeNode.cpp
+++ NodeProject/Model/TreeNode.cpp
@@ -9,7 +9,28 @@
 #include "TreeNode.h"
 
 template <class Type>
-TreeNode<Type>::TreeNode()
+TreeNode<Type>:: TreeNode()
 {
-    
+    this->value = nullptr;
+    this->parent = nullptr;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+};
+
+template <class Type>
+TreeNode<Type>:: TreeNode(const Type& value)
+{
+    this->value = value;
+    this->parent = nullptr;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+};
+
+template <class Type>
+TreeNode<Type>:: TreeNode(const Type& value, TreeNode<Type>* parent)
+{
+    this->value = value;
+    this->parent = parent;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
 };
