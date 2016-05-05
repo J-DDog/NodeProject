3e8c7a71fff61f56aad622e6bfc2d45166d92f91
Traitortots <jker3169@740s-pl-122.local>
2016-05-05 08:40:23 -0600

    added hash table

---
+++ NodeProject/Model/HashTable.cpp
@@ -0,0 +1,9 @@
+//
+//  HashTable.cpp
+//  NodeProject
+//
+//  Created by Kerr, Jared on 5/5/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "HashTable.h"
---
+++ NodeProject/Model/HashTable.h
@@ -0,0 +1,34 @@
+//
+//  HashTable.hpp
+//  NodeProject
+//
+//  Created by Kerr, Jared on 5/5/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+template <class Type>
+class HashTable
+{
+public:
+    HashTable();
+    ~HashTable();
+    void add(const Type& value);
+    bool remove(const Type& value);
+    bool contains(const Type& value);
+    
+private:
+    int capacity;
+    double efficiencyPercentage;
+    int size;
+    Type * internalStorage;
+    
+    int findPosition(const Type& value);
+    int handleCollision(const Type& value);
+    void updateSize();
+    
+};
+
+#endif /* HashTable_hpp */
